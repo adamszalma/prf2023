c1786b7f8ecb252c16d28c3dc58593f787bb3bfc
Szalma Ádám <adam.szalma@icbtech.rs>
2023-05-09 20:51:47 +0200

    add pets listing - frontend

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ConnectionService } from 'src/app/utils/connection.service';
+import { Pet } from 'src/models/pet.model';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  petsArray: Pet[] = [];
 
+  constructor (private connectionService: ConnectionService) {
+
+  }
+
+  ngOnInit() {
+    this.updatePetsArray();
+  }
+
+  updatePetsArray() {
+    this.connectionService.getPets().subscribe(result => {
+      while (this.petsArray.length > 0) {
+        this.petsArray.pop();
+      }
+
+      (result as any).forEach((element: any) => {
+        const tmpPet = new Pet(element.id, element.petName, element.petType);
+        this.petsArray.push(tmpPet);
+      });
+    }, error => {
+      console.log(error);
+    });
+  }
 }
---
+++ frontend/src/models/pet.model.ts
@@ -0,0 +1,11 @@
+export class Pet {
+    id: string;
+    petName: string;
+    petType: string;
+
+    constructor(id: string, petName: string, petType: string) {
+        this.id = id;
+        this.petName = petName;
+        this.petType = petType;
+    }
+}
